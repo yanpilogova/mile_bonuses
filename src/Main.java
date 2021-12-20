3b0fbf03272b2c656007a05a6233951bea421758
yanpilogova <anpilogova1986@gmail.com>
2021-12-20 16:58:22 +0700

    added gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       int costTicket = 105;
+       int mileCost = 20;
+       int totalMile = costTicket/mileCost;
+        System.out.println( "Полученные мили:" + totalMile);
+
+    }
+}
