a04598df2b538ee36350effb72c47e497cfae694
yanpilogova <anpilogova1986@gmail.com>
2021-12-26 10:59:06 +0700

    the code formatting

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-       int costTicket = 105;
-       int mileCost = 20;
-       int totalMile = costTicket/mileCost;
-        System.out.println( "Полученные мили:" + totalMile);
+        int costTicket = 105;
+        int mileCost = 20;
+        int totalMile = costTicket / mileCost;
+        System.out.println("Полученные мили:" + totalMile);
 
     }
 }
